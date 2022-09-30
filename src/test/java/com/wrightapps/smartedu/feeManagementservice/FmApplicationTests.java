c13513d8b403a53b21317fd84c5f328ccea2f6e6
MukundBhonagiri <mukundbhonagiri@gmail.com>
2022-09-30 23:02:39 +0530

    test package change

--- src/test/java/com/wrightapps/smartedu/feeManagementservice/FmApplicationTests.java
+++ src/test/java/com/wrightapps/smartedu/feeManagementservice/FmApplicationTests.java
@@ -1,4 +1,4 @@
-package com.feeManagement.fm;
+package com.wrightapps.smartedu.feeManagementservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
